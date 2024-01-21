5e2754a75f3d4be956f1d9aa5d453beb41fb5884
LampaMagica <77008202+LampaMagica@users.noreply.github.com>
2024-01-21 10:37:21 +0100

    Adding multi link api
    Optimizing main
    adding multi link api

--- get_data.py
+++ get_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from datetime import datetime
-
 # get_base_data = pd.read_csv('src\data_small\stations.txt',skiprows=17)
 # # print(get_base_data.columns)
 # data_test = get_base_data.loc[get_base_data['STANAME                                 ']=='VAEXJOE                                 ']
@@ -34,14 +33,55 @@ def get_file(id):
     df = pd.read_csv(f'src\data_small\{station_name}.txt',skiprows=20,parse_dates=['    DATE'])
     return df
 
+def get_file_set_index(id,index):
+    station_name = get_station(id)
+    df = pd.read_csv(f'src\data_small\{station_name}.txt',skiprows=20,parse_dates=['    DATE'],index_col=index)
+    df_filtred = df.loc[df['   TG'] != -9999]
+    return df_filtred
+
 #Get The Row
 def get_row_by_date(date,id):
     df = get_file(id)
-    df_row = df.loc[df["    DATE"] == date]
-    return df_row.iloc[0].to_dict()
+
+    #Removing fake data
+    df_row = df.loc[df['   TG'] != -9999]
+    
+    #Init data list
+    data = []
+
+    #testing if user enter full date or just a year
+    if len(date) > 4:
+
+        #Filtre que the row of the date
+        df_row = df.loc[df["    DATE"] == date]
+        temperature = df_row.get('   TG') / 10
+        data_row = {
+            "Date":df_row.get('    DATE').squeeze(),
+            "Station_ID":int(df_row.get('STAID').squeeze()),
+            "Temp":float(temperature)
+        }
+        return data_row
+    
+    elif len(date) == 4:
+
+        #Adding a column year
+        df_row.loc[:,'Year'] = df_row['    DATE'].dt.year
+
+        #Filtre for the Year
+        df_data_date = df_row.loc[df_row['Year'] == int(date)]
+
+        #Loop to regroupe data in a list
+        for i,row in df_data_date.iterrows():
+            data_row = {
+            "Date":row.get('    DATE'),
+            "Station_ID":row.get('STAID'),
+            "Temp":row.get('   TG') / 10
+            }
+            data.append(data_row)
+        return data
 
 
-def get_station():
+def get_station_main_file():
     df = pd.read_csv('src\data_small\stations.txt', skiprows=17)
     return df
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,url_for
 import get_data
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -17,20 +18,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    station = get_data.get_station()
+    station = get_data.get_station_main_file()
     html_rendered = render_template('home.html', station=station.to_html())
     return html_rendered
 
-@app.route('/api/v1/<date>/<station>')
+@app.route('/api/v1/<station>/<date>')
 def api_v1(date,station):
-    row_data = get_data.get_row_by_date(date,station)
-    temperature = row_data.get('   TG') / 10
-    data = {
-        "date":row_data.get('    DATE'),
-        "station":row_data.get('STAID'),
-        "temperature":temperature
-    }
+    get_data_by_date_var = get_data.get_row_by_date(date,station)
+    return get_data_by_date_var
+
+    
+@app.route('/api/v1/<station>')
+def data_station(station):
+    data_raw = get_data.get_file_set_index(station,None)
+    data = []
+    for i, row in data_raw.iterrows():
+        data_structred_time = datetime.strptime(str(row['    DATE']),"%Y-%m-%d %H:%M:%S")
+        date_without_hours = data_structred_time.strftime('%Y-%m-%d')
+        row_data = {
+            'Station_ID' : float(row['STAID']),
+            'Date' : date_without_hours,
+            'Temp' : float(row['   TG'] / 10)
+        }
+        data.append(row_data)
     return data
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
